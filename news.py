3aca857cea6eda4bb37761f17e826342efd0c6d5
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-08-03 22:59:03 -0300

    scraping category news

--- news.py
+++ news.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.by import By
 import pandas as pd
 
 website = "https://gamerant.com/"
@@ -9,24 +10,26 @@ service = Service(executable_path=path)
 driver = webdriver.Firefox(service=service)
 driver.get(website)
 
-containers = driver.find_elements(by="xpath", value='//div[@class="w-display-card-content"]')
+news_containers = driver.find_elements(by="xpath", value='//div[@class="w-display-card-content"]')
 
 categories = list()
-titles = []
-links = []
+titles = list()
+links = list()
 
-for container in containers:
+for news in news_containers:
+    category = news.find_element(by='xpath', value='//div[@class="w-display-card-content"]\n'
+                                                   '//div[@class="w-display-card-category"]/a').text
+    title = news.find_element(by='xpath', value='./h5/a').text
+    link = news.find_element(by='xpath', value='./h5/a').get_attribute('href')
 
-    title = container.find_element(by='xpath', value='./h5/a').text
-    link = container.find_element(by='xpath', value='./h5/a').get_attribute('href')
-
+    categories.append(category)
     titles.append(title)
     links.append(link)
 
-news_dict = {"title": titles, "links": links}
+news_dict = {"Categories": categories, "Titles": titles, "Links": links}
 
 recent_news = pd.DataFrame(news_dict)
-recent_news.to_csv("recent_news.csv")
+recent_news.to_csv("gamerant.csv")
 
 print("Process: Collecting news data....")
 
