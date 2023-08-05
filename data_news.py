063943f19df066f5be99999fdcf53053aac36e39
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-08-05 15:18:20 -0300

    creat Grid class to define news grid user interface

--- data_news.py
+++ data_news.py
@@ -1,13 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.common.by import By
 import pandas as pd
+from tkinter import *
+from Grid import Grid
 
 website = 'https://gamerant.com/'
 path = 'geckodriver.exe'
 
-#active headless-mode
+# active headless-mode
 options = Options()
 options.headless = True
 
@@ -17,25 +18,21 @@ driver.get(website)
 
 news_containers = driver.find_elements(by="xpath", value='//div[@class="w-display-card-content"]')
 
-categories = list()
 titles = list()
 links = list()
 
 for news in news_containers:
-    category = news.find_element(by='xpath', value='//div[@class="w-display-card-content"]\n'
-                                                   '//div[@class="w-display-card-category"]/a').text
     title = news.find_element(by='xpath', value='./h5/a').text
     link = news.find_element(by='xpath', value='./h5/a').get_attribute('href')
-
-    categories.append(category)
     titles.append(title)
     links.append(link)
 
-news_dict = {"Categories": categories, "Titles": titles, "Links": links}
 
-recent_news = pd.DataFrame(news_dict)
-recent_news.to_csv("gamerant.csv")
-
-print("Process: Collecting news data....")
+# export news as .csv file
+news_dict = {"Titles": titles, "Links": links}
+latest_news = pd.DataFrame(news_dict)
+latest_news.to_csv("gamerant.csv")
 
 driver.quit()
+
+
